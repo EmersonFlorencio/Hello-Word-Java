ac4e0fdeaf5ded9864534e25cf56359a4ff1a2eb
Emerson Florencio <emersonflorencio@hotmail.com>
2023-04-26 21:36:37 -0300

    correct commit

---
+++ HelloWorld/src/helloWorld/HelloWorldJava.java
@@ -0,0 +1,15 @@
+package helloWorld;
+
+public class HelloWorldJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World!!!");
+		System.out.print("test new language");
+	}
+	
+	public static void test(String[] args) {
+		System.out.print("teste!!!");
+	}
+
+}
